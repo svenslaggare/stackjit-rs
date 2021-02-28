ff78a57a3427b73293c2870ce5ffd9d2ed570ad6
Anton Jansson <anton_jansson@live.se>
2021-02-28 21:39:19 +0100

    Generic 'new_object' function in memory manager.

--- src/runtime/memory.rs
+++ src/runtime/memory.rs
@@ -20,19 +20,27 @@ impl MemoryManager {
     pub fn new_array(&mut self, type_instance: &Type, length: i32) -> *mut std::ffi::c_void {
         let element_type = type_instance.element_type().expect("unexpected type");
 
-        let data_size = length as usize * element_type.size();
-        let array_size = array::LENGTH_SIZE + data_size;
-        let obj_ptr = self.heap.allocate(array_size).unwrap();
+        let array_size = array::LENGTH_SIZE + length as usize * element_type.size();
+        let obj_ptr = self.new_object(array_size);
+
         unsafe {
             *(obj_ptr as *mut i32) = length;
-
-            let data_ptr = obj_ptr.add(array::LENGTH_SIZE) as *mut u8;
-            for i in 0..array_size as isize {
-                *data_ptr.offset(i) = 0;
-            }
         }
 
         println!("Allocated array (type: {}, length: {}, size: {}): 0x{:x}", type_instance, length, array_size, obj_ptr as u64);
+        obj_ptr
+    }
+
+    fn new_object(&mut self, size: usize) -> *mut std::ffi::c_void {
+        let obj_ptr = self.heap.allocate(size).unwrap();
+
+        unsafe {
+            let obj_ptr = obj_ptr as *mut u8;
+            for i in 0..size as isize {
+                *obj_ptr.offset(i) = 0;
+            }
+        }
+
         obj_ptr
     }
 }
